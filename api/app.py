701422fd1261690d2221a8bd8c99a16cbce90501
Bogdan Gaza <bc.gaza@gmail.com>
2016-07-16 14:12:44 -0700

    Tabs -> Spaces

--- api/app.py
+++ api/app.py
@@ -19,32 +19,32 @@ def index():
 # or GET /nlp?text=i+have+a+dog - which is text input
 @app.route('/nlp', methods=['GET', 'POST'])
 def nlp():
-	textIn = request.args.get('text','')
-	if textIn:
-		#skip s2t
-		textOut = textIn
-	else:
-		if 'file' not in request.files:
-			abort(400)
-		audioIn = request.files['audio_wav_file']
-		if audioIn and allowed_file(audioIn.filename):
-			audioFilename = secure_filename(audioIn.filename)
-			audioIn.save(os.path.join(app.config['UPLOAD_FOLDER'], audioFilename))
-		# TODO: chunk audio if needed, apply s2t on audio
-		# textOut = s2t(audioIn)
-	
-	# TODO:
-	# Fire off job to build entity graph - think about concurency?
-	# do syncronously for now
+    textIn = request.args.get('text','')
+    if textIn:
+        #skip s2t
+        textOut = textIn
+    else:
+        if 'file' not in request.files:
+            abort(400)
+        audioIn = request.files['audio_wav_file']
+        if audioIn and allowed_file(audioIn.filename):
+            audioFilename = secure_filename(audioIn.filename)
+            audioIn.save(os.path.join(app.config['UPLOAD_FOLDER'], audioFilename))
+        # TODO: chunk audio if needed, apply s2t on audio
+        # textOut = s2t(audioIn)
 
-	textOut = "I have a dog.  Its name is Lisa." # sample text
+    # TODO:
+    # Fire off job to build entity graph - think about concurency?
+    # do syncronously for now
 
-	# return 200 and audio text
-	return jsonify(text=textOut)
+    textOut = "I have a dog.  Its name is Lisa." # sample text
+
+    # return 200 and audio text
+    return jsonify(text=textOut)
 
 @app.route('/graph', methods=['GET'])
 def graph():
-	return jsonify(nodes=set(['test_node']))
+    return jsonify(nodes=set(['test_node']))
 
 def allowed_file(filename):
     return '.' in filename and \
